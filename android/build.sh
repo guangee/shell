59cb91cecff4cad1c55a65a14edb9ecbc78a0df6
zziaguan <roma@guanweiming.com>
2018-12-26 22:17:53 +0800

    ok

--- android/build.sh
+++ android/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 work_dir="project"
-project="git@git.coding-space.cn:coding/android-upload-connect.git"
+project="https://git.coding-space.cn/guange/demo-android.git"
 echo '清理工作空间'
 rm -rf ${work_dir}
 echo "克隆代码"
@@ -9,7 +9,8 @@ echo "克隆代码"
 git clone ${project} ${work_dir}
 
 cd ${work_dir}
-docker run --tty --interactive --volume=$(pwd):/opt/workspace --workdir=/opt/workspace --rm jacekmarchwicki/android:java7  /bin/sh -c "./gradlew build"
+chmod 777 gradlew
+docker run --tty --interactive --volume=$(pwd):/opt/workspace --workdir=/opt/workspace --rm jacekmarchwicki/android  /bin/sh -c "./gradlew build"
 cd ..
 
 
