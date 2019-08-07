5c62067d35fc0eb971a73abc12f023a2bdd40442
zziaguan <roma@guanweiming.com>
2019-08-07 15:38:31 +0800

    ok

--- android/jenkins/ajenkins_install.sh
+++ android/jenkins/ajenkins_install.sh
@@ -13,7 +13,10 @@ unzip sdk-tools-linux-4333796.zip
 
 RUN yes | /opt/android/tools/bin/sdkmanager "platform-tools"
 RUN yes | /opt/android/tools/bin/sdkmanager "build-tools;28.0.2"
+RUN yes | /opt/android/tools/bin/sdkmanager "build-tools;28.0.3"
 RUN yes | /opt/android/tools/bin/sdkmanager "platforms;android-28"
+
+# /opt/android/tools/bin/sdkmanager --licenses
 
 
 wget http://mirror.bit.edu.cn/apache/tomcat/tomcat-8/v8.5.37/bin/apache-tomcat-8.5.37.tar.gz
