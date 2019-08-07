03f5de9792f60ff0a516c1247821229dbb2bd823
zziaguan <roma@guanweiming.com>
2019-08-07 23:03:09 +0800

    自动提交

--- android/jenkins/ajenkins_install.sh
+++ android/jenkins/ajenkins_install.sh
@@ -4,19 +4,19 @@ ANDROID_HOME=/opt/android
 
 apt update
 apt upgrade -y
-apt install wget unzip
+apt install wget unzip -y
 mkdir /opt/android && cd /opt/android
 wget https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
 unzip sdk-tools-linux-4333796.zip
 
 
 
-RUN yes | /opt/android/tools/bin/sdkmanager "platform-tools"
-RUN yes | /opt/android/tools/bin/sdkmanager "build-tools;28.0.2"
-RUN yes | /opt/android/tools/bin/sdkmanager "build-tools;28.0.3"
-RUN yes | /opt/android/tools/bin/sdkmanager "platforms;android-28"
+yes |  /opt/android/tools/bin/sdkmanager "platform-tools"
+yes |  /opt/android/tools/bin/sdkmanager "build-tools;28.0.2"
+yes |  /opt/android/tools/bin/sdkmanager "build-tools;28.0.3"
+yes |  /opt/android/tools/bin/sdkmanager "platforms;android-28"
 
-# /opt/android/tools/bin/sdkmanager --licenses
+yes | /opt/android/tools/bin/sdkmanager --licenses
 
 
 wget http://mirror.bit.edu.cn/apache/tomcat/tomcat-8/v8.5.37/bin/apache-tomcat-8.5.37.tar.gz
