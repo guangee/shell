b0f1b9973a797c76b4877c354609856578776b44
关卫明 <roma@guanweiming.com>
2023-02-15 10:48:52 +0800

    镜像同时往dockerhub推送

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ build() {
   path=$2
   docker build -t registry.cn-zhangjiakou.aliyuncs.com/tulan/${image} ./${path}
   docker push registry.cn-zhangjiakou.aliyuncs.com/tulan/${image}
+  docker build -t ${docker_hub_username}/${image} ./${path}
+  docker push ${docker_hub_username}/${image}
 }
 
 build "$1" "$2"
