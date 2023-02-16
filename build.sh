d58a3468e0cd57760608dc0088b7d310cb2235be
关卫明 <roma@guanweiming.com>
2023-02-16 14:38:58 +0800

    github build

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 build() {
   image=$1
   path=$2
-  docker build -t registry.cn-zhangjiakou.aliyuncs.com/tulan/${image} ./${path}
-  docker push registry.cn-zhangjiakou.aliyuncs.com/tulan/${image}
+#  docker build -t registry.cn-zhangjiakou.aliyuncs.com/tulan/${image} ./${path}
+#  docker push registry.cn-zhangjiakou.aliyuncs.com/tulan/${image}
   docker build -t ${docker_hub_username}/${image} ./${path}
   docker push ${docker_hub_username}/${image}
 }
